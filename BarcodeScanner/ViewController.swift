c6568e1ffcdb89debe960cee7a00c671578ffef7
k.khristenko <k.v.hrist@gmail.com>
2022-06-23 12:02:18 +0300

    Initial commit

--- BarcodeScanner/ViewController.swift
+++ BarcodeScanner/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func btn(_ sender: UIButton) {
+        
+        SPQRCode.scanning(handled: { data, controller in
+                    self.bindingData(data: data)
+                    controller.dismiss(animated: true)
+                }, on: self)
+            }
+            func bindingData(data: SPQRCodeData) {
+                
+                switch data {
+                case .url(let uRL):
+                    label.text = "\(uRL)"
+                case .text(let string):
+                    label.text = string
+                case .ethWallet(let string):
+                    print(string)
+                }
+                label.isHidden = false
+                
+    }
+    
 }
 
